56534eea297a51499b3b106b9a3f0abbeb0bad98
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-02-04 07:36:16 +0700

    💄 Add Header and Quiz UI
    Add Header and Quiz UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import React from 'react'
+import Header from './components/Header'
+import Quiz from './components/Quiz'
 
-export default App;
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <main>
+      <Quiz/>
+    </main>
+    </>
+  )
+}
+
+export default App
